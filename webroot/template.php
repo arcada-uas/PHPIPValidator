21b6b5c51e245c9e7a2f81f07a7e79b96254a247
Johannes Edgren <edgrenjo@arcada.fi>
2022-03-17 16:16:14 +0200

    Updated style and config template

--- webroot/template.php
+++ webroot/template.php
@@ -4,22 +4,23 @@
 
 <div class="splash"><h1>Arcada Attendance</h1></div>
 
-<section  class="flex">
-<div>
-    <!--Show user name when he visits the site-->
-    <h2>Hello <?= $template['displayName'] ?></h2>
-    <!--Check if user is already registered-->
-    <?php if (!$template['isRegistered']) : ?>
-        <!--Show user a button to register for the supplied course-->
-        <h2>Register for <?= $template['courses'][$template['course']] ?> :: <?= date("Y-m-d") ?></h2>
-        <form method="POST">
-            <input type="submit" name="submitAttendance" value="Register">
-        </form>
-    <?php else : ?>
-        <!--If user has already registered for todays lecture inform him of when he did-->
-        <h2>You registered for todays <?= $template['courses'][$template['isRegistered']['course']] ?> lecture at <?= $template['isRegistered']['date'] ?></h2>
-    <?php endif; ?>
-</div>
+<section class="flex">
+    <div>
+        <!--Show user name when he visits the site-->
+        <h2>Hello <?= $template['displayName'] ?></h2>
+        <!--Check if user is already registered-->
+        <?php if (!$template['isRegistered']) : ?>
+            <!--Show user a button to register for the supplied course-->
+            <h2>Register to "<?= $template['courses'][$template['course']] ?>" <?= date("Y-m-d") ?> Lecture</h2>
+            <form method="POST">
+                <input class="button" type="submit" name="submitAttendance" value="Register">
+            </form>
+        <?php else : ?>
+            <!--If user has already registered for todays lecture inform him of when he did-->
+            <h2>You registered for todays <?= $template['courses'][$template['isRegistered']['course']] ?> lecture
+                at <?= $template['isRegistered']['date'] ?></h2>
+        <?php endif; ?>
+    </div>
 </section>
 
 </body>
